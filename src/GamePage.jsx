6b070dbdfa1cc6ea20d23a07bac524f1e250aefc
Howon Jung <jhwlds@gmail.com>
2025-04-04 21:29:47 -0600

    basic structure

--- src/GamePage.jsx
+++ src/GamePage.jsx
@@ -1,9 +1,20 @@
 import { useState } from 'react';
 
 function GamePage({ word }) {
-
   return (
-    true
+    <div>
+      <h2>Guess the World!</h2>
+
+      <p>displaying the guessing process</p>
+
+      <input 
+        type="text"
+        maxLength={1}
+      />
+
+      <p>any massage for the progress</p>
+
+    </div>
   );
 }
 
